db79c80e17916067446c2438a88fc91903d75469
ipeerless <ipeerless0@gmail.com>
2022-09-12 18:56:15 +0200

    Initial Commit

---
+++ User-location/User_locationApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_locationApp.swift
+//  User-location
+//
+//  Created by ipeerless on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct User_locationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
